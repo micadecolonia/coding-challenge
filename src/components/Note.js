99d2f574de126dd51934d345745d571945cee456
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-28 20:29:33 -0300

    First draft of note w/dynamic data + props

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,5 +1,23 @@
-const Note = () => {
-  return <div className="note"></div>  
+import { MdDeleteForever } from 'react-icons/md';
+import PropTypes from 'prop-types';
+
+// eslint-disable-next-line no-unused-vars
+function Note({ id, text, date }) {
+  return (
+    <div className="note">
+      <div className="note-header">
+        <small>{date}</small>
+        <MdDeleteForever className="delete-icon" size="1.3em" />
+      </div>
+      <span>{text}</span>
+    </div>
+  );
+}
+
+Note.propTypes = {
+  id: PropTypes.number.isRequired,
+  text: PropTypes.string.isRequired,
+  date: PropTypes.string.isRequired,
 };
 
 export default Note;
--- src/components/NotesList.js
+++ src/components/NotesList.js
@@ -1,9 +1,21 @@
-const NotesList = () => {
+import PropTypes from 'prop-types';
+import Note from './Note';
+import AddNote from './AddNote';
+
+function NotesList({ notes }) {
   return (
-    <div className='notes-list'>
-      <Note />
+    <div className="notes-list">
+      {notes.map((note) => (
+        <Note id={note.id} text={note.text} date={note.date} />
+
+      ))}
+      <AddNote />
     </div>
   );
+}
+
+NotesList.propTypes = {
+  notes: PropTypes.element.isRequired,
 };
 
 export default NotesList;
