bb72113e065103aabe7622edfad054cc578dcc52
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-29 22:26:27 -0300

    Working on notes in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
-const App = () => {
-  return(<p></p>)
-};
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import NotesList from './components/NotesList';
+
+function App() {
+  // eslint-disable-next-line no-unused-vars
+  const [notes, setNotes] = useState([{
+    id: nanoid(),
+    text: 'Hello world - This is my first note!',
+    date: '15/04/2022',
+  },
+  {
+    id: nanoid(),
+    text: 'Hello world - This is my second note!',
+    date: '16/04/2022',
+  },
+  {
+    id: nanoid(),
+    text: 'Hello world - This is my third note!',
+    date: '17/04/2022',
+  }]);
+
+  const addNote = (text) => {
+    console.log(text);
+  };
+
+  return (
+    <div className="container">
+      <NotesList notes={notes} handleAddNote={addNote} />
+    </div>
+  );
+}
 
 export default App;
