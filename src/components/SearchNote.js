8758bf1dd7c169047aae25733ba640526110dc76
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-29 23:46:12 -0300

    Search bar created

--- src/components/SearchNote.js
+++ src/components/SearchNote.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { MdSearch } from 'react-icons/md';
 
 function SearchNote() {
-  return <div />;
+  return (
+    <div className="search">
+      <MdSearch className="search-icons" size="1.3em" />
+      <input type="text" placeholder="Search for a note" />
+    </div>
+  );
 }
 
 export default SearchNote;
