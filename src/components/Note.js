1a14cb4c56d338fb3e792b532e6c04ca03501eac
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-31 16:21:51 -0300

    Added possibility to edit the note

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,18 +1,35 @@
-import { MdEdit, MdDeleteForever } from 'react-icons/md';
+import { MdDeleteForever, MdEdit } from 'react-icons/md';
 import PropTypes from 'prop-types';
+import EditNote from './EditNote';
 
+// eslint-disable-next-line no-unused-vars
 function Note({
-  id, text, date, handleDeleteNote,
+  id, text, date, handleDeleteNote, isEdit, handleIsEdit, handleEditNote,
 }) {
   return (
-    <div className="note">
-      <div className="note-header">
-        <small>{date}</small>
-        <MdDeleteForever onClick={() => handleDeleteNote(id)} className="delete-icon" size="1.3em" />
-        <MdEdit className="edit-icon" size="1.3em" />
-      </div>
-      <span id="note-text">{text}</span>
-    </div>
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    <>
+      {
+      isEdit
+        ? (
+          <EditNote
+            note={{ id, text, date }}
+            handleEditNote={handleEditNote}
+            handleIsEdit={handleIsEdit}
+          />
+        )
+        : (
+          <div className="note">
+            <div className="note-header">
+              <small>{date}</small>
+              <MdDeleteForever onClick={() => handleDeleteNote({ id })} className="delete-icon" size="1.3em" />
+              <MdEdit onClick={() => handleIsEdit(true)} className="edit-icon" size="1.3em" />
+            </div>
+            <span>{text}</span>
+          </div>
+        )
+    }
+    </>
   );
 }
 
@@ -21,6 +38,9 @@ Note.propTypes = {
   text: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
   handleDeleteNote: PropTypes.func.isRequired,
+  handleEditNote: PropTypes.func.isRequired,
+  isEdit: PropTypes.bool.isRequired,
+  handleIsEdit: PropTypes.func.isRequired,
 };
 
 export default Note;
