e2de17c7c86537a8a06ab6fd12baf4ebdc42fb5e
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-31 16:27:52 -0300

    App.js updated with react-router implemented

--- src/App.js
+++ src/App.js
@@ -1,74 +1,28 @@
-import { useEffect, useState } from 'react';
-import { nanoid } from 'nanoid';
-import NotesList from './components/NotesList';
-import SearchNote from './components/SearchNote';
+import { useState } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
 import Header from './components/Header';
+import Dashboard from './pages/Dashboard';
+import Trash from './pages/Trash';
 
 function App() {
-  const [notes, setNotes] = useState([{
-    id: nanoid(),
-    text: 'Hello world - This is my first note!',
-    date: '15/04/2022',
-  },
-  {
-    id: nanoid(),
-    text: 'Hello world - This is my second note!',
-    date: '16/04/2022',
-  },
-  {
-    id: nanoid(),
-    text: 'Hello world - This is my third note!',
-    date: '17/04/2022',
-  }]);
-
-  const [searchText, setSearchText] = useState('');
-
   const [darkMode, setDarkMode] = useState(false);
-
-  useEffect(() => {
-    const savedNotes = JSON.parse(
-      localStorage.getItem('post-it-notes-app-data'),
-    );
-
-    // If there are saved notes
-    if (savedNotes) {
-      setNotes(savedNotes);
-    }
-  }, []);
-
-  useEffect(() => {
-    localStorage.setItem(
-      'post-it-notes-app-data',
-      JSON.stringify(notes),
-    );
-  }, [notes]);
-
-  const addNote = (text) => {
-    const date = new Date();
-    const newNote = {
-      id: nanoid(),
-      text,
-      date: date.toLocaleDateString(),
-    };
-    const newNotes = [...notes, newNote];
-    setNotes(newNotes);
-  };
-
-  const deleteNote = (id) => {
-    const newNotes = notes.filter((note) => note.id !== id);
-    setNotes(newNotes);
-  };
 
   return (
     <div className={`${darkMode && 'dark-mode'}`}>
       <div className="container">
-        <Header handleToggleDarkMode={setDarkMode} />
-        <SearchNote handleSearchNote={setSearchText} />
-        <NotesList
-          notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))}
-          handleAddNote={addNote}
-          handleDeleteNote={deleteNote}
-        />
+        <Router>
+          <div className="App">
+            <Header handleToggleDarkMode={setDarkMode} />
+            <Switch>
+              <Route exact path="/" component={Dashboard} />
+              <Route path="/trash" component={Trash} />
+            </Switch>
+          </div>
+        </Router>
       </div>
     </div>
 
