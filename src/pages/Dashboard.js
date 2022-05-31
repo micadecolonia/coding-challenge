ba5c64742a5333afe98dd83117a4c9970b9c62ae
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-31 16:26:53 -0300

    Main dashboard of the app added

---
+++ src/pages/Dashboard.js
@@ -0,0 +1,53 @@
+import { useState } from 'react';
+import { nanoid } from 'nanoid';
+import { useSelector, useDispatch } from 'react-redux';
+import NotesList from '../components/NotesList';
+import SearchNote from '../components/SearchNote';
+import { addNote, deleteNote, editNote } from '../store/notesSlice';
+import AddNote from '../components/AddNote';
+
+function Dashboard() {
+  const { notes } = useSelector((state) => state.notes);
+  const dispatch = useDispatch();
+  const [searchText, setSearchText] = useState('');
+  const [isEdit, setIsEdit] = useState(false);
+  const date = new Date();
+
+  const addN = (text) => {
+    const newNote = {
+      id: nanoid(),
+      text,
+      date: date.toLocaleDateString(),
+    };
+    dispatch(addNote(newNote));
+  };
+
+  const deleteN = (note) => {
+    dispatch(deleteNote(note));
+  };
+
+  const editN = (noteEdit) => {
+    dispatch(editNote(noteEdit));
+  };
+
+  return (
+    <>
+      <SearchNote handleSearchNote={setSearchText} />
+      {notes && notes.length > 0
+        ? (
+          <NotesList
+            notes={notes.filter((note) => note.text.toLowerCase().includes(searchText))}
+            handleAddNote={addN}
+            handleDeleteNote={deleteN}
+            handleEditNote={editN}
+            isEdit={isEdit}
+            handleIsEdit={setIsEdit}
+          />
+        )
+        : (<AddNote handleAddNote={addN} />)}
+
+    </>
+  );
+}
+
+export default Dashboard;
