764cef2885dc6db627f8a9d8cd2e3d73af3d0ead
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-31 16:17:40 -0300

    Added trash can icon to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,16 @@
 import PropTypes from 'prop-types';
 import React from 'react';
-import { MdOutlineDarkMode } from 'react-icons/md';
+import { MdOutlineDarkMode, MdDeleteForever } from 'react-icons/md';
+import { useHistory } from 'react-router-dom';
 
 function Header({ handleToggleDarkMode }) {
+  const history = useHistory();
+
   return (
     <div className="header">
       <h1>My notes dashboard</h1>
       <MdOutlineDarkMode onClick={() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)} className="save" />
+      <MdDeleteForever onClick={() => history.push('/trash')} className="delete-icon" size="2em" color="red" />
     </div>
   );
 }
