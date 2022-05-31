7000fa0b1b7f7f6502dbaa5fd721e08f4c000052
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-31 20:12:52 -0300

    Added dynamic h1 for both pages

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,26 @@
 import PropTypes from 'prop-types';
 import React from 'react';
 import { MdOutlineDarkMode, MdDeleteForever } from 'react-icons/md';
-import { useHistory } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
+
+function HeaderView() {
+  const location = useLocation();
+  switch (location.pathname) {
+    case '/':
+      return 'My notes dashboard';
+    case '/trash':
+      return 'Deleted notes';
+    default:
+      return 'I do not know how you got here';
+  }
+}
 
 function Header({ handleToggleDarkMode }) {
   const history = useHistory();
 
   return (
     <div className="header">
-      <h1 id="page-title">My notes dashboard</h1>
+      <h1 id="page-title">{HeaderView()}</h1>
       <MdOutlineDarkMode onClick={() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)} className="moon" />
       <MdDeleteForever onClick={() => history.push('/trash')} className="delete-icon" size="2em" color="red" />
     </div>
