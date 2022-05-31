95dfc4e3e27e6e7a86190612efdbfe73eb3d4482
Micaela Rodriguez <micadecolonia@gmail.com>
2022-05-31 16:24:40 -0300

    Initial state of the notes-trash

---
+++ src/store/initState.js
@@ -0,0 +1,32 @@
+import { nanoid } from 'nanoid';
+
+const notes = [{
+  id: nanoid(),
+  text: 'Hello world - This is my first note!',
+  date: '15/04/2022',
+  isTrashed: 'false',
+},
+{
+  id: nanoid(),
+  text: 'Hello world - This is my second note!',
+  date: '16/04/2022',
+  isTrashed: 'false',
+},
+{
+  id: nanoid(),
+  text: 'Hello world - This is my third note!',
+  date: '17/04/2022',
+  isTrashed: 'false',
+}];
+
+export const initialNoteState = {
+  lastNoteCreatedAt: null,
+  totalNotes: notes.length,
+  notes,
+};
+
+export const initialTrashState = {
+  lastNoteCreatedAt: null,
+  totalNotes: 0,
+  trash: [],
+};
