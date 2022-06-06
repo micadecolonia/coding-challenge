332bb79302b5b9a67fd2c13305a41b6a03c0f558
Micaela Rodriguez <micadecolonia@gmail.com>
2022-06-06 00:06:59 -0300

    Added css to only one note left in the trash bin

--- src/components/Note.js
+++ src/components/Note.js
@@ -1,13 +1,16 @@
 import { useState } from 'react';
-import { MdDeleteForever, MdEdit } from 'react-icons/md';
+import { MdDeleteForever, MdEdit, MdUndo } from 'react-icons/md';
 import PropTypes from 'prop-types';
+import { useSelector } from 'react-redux';
 import EditNote from './EditNote';
 
 // eslint-disable-next-line no-unused-vars
 function Note({
-  note, handleDeleteNote, isEdit, handleIsEdit, handleEditNote,
+  note, handleDeleteNote, handleRemoveFromTrash, isEdit, isDelete, handleIsEdit, handleEditNote,
 }) {
   const [isSelectedId, setIselectedId] = useState(null);
+  const { totalNotes } = useSelector((state) => state.trash);
+  const isOnlyOneNoteLeft = totalNotes === 1;
 
   return (
     // eslint-disable-next-line react/jsx-no-useless-fragment
@@ -23,19 +26,27 @@ function Note({
           />
         )
         : (
-          <div className="note">
+          <div className={`note ${isOnlyOneNoteLeft ? 'deleted-note' : ''}`} style={{ backgroundColor: note.color }}>
             <div className="note-header">
               <small>{note.date}</small>
               {/* // TODO If it is deleting, do not show edit or delete icons */}
-              <MdDeleteForever onClick={() => handleDeleteNote(note)} className="delete-icon" size="1.3em" />
-              <MdEdit
-                onClick={() => {
-                  setIselectedId(note.id);
-                  handleIsEdit(true);
-                }}
-                className="edit-icon"
-                size="1.3em"
-              />
+              {!isDelete ? (
+
+                <>
+
+                  <MdDeleteForever onClick={() => handleDeleteNote(note)} className="delete-icon" size="1.3em" />
+                  <MdEdit
+                    onClick={() => {
+                      setIselectedId(note.id);
+                      handleIsEdit(true);
+                    }}
+                    className="edit-icon"
+                    size="1.3em"
+                  />
+                </>
+              )
+                : (<MdUndo onClick={() => handleRemoveFromTrash(note)} className="save" />)}
+
               {/* // TODO Add button to take out from the trash */}
             </div>
             <span>{note.text}</span>
@@ -51,10 +62,13 @@ Note.propTypes = {
     id: PropTypes.string.isRequired,
     date: PropTypes.string.isRequired,
     text: PropTypes.string.isRequired,
+    color: PropTypes.string.isRequired,
   }).isRequired,
   handleDeleteNote: PropTypes.func.isRequired,
+  handleRemoveFromTrash: PropTypes.func.isRequired,
   handleEditNote: PropTypes.func.isRequired,
   isEdit: PropTypes.bool.isRequired,
+  isDelete: PropTypes.bool.isRequired,
   handleIsEdit: PropTypes.func.isRequired,
 };
 
