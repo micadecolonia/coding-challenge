987ae6a4852b7180ed5068a42adc3da9aa7d0aec
Micaela Rodriguez <micadecolonia@gmail.com>
2022-06-08 16:28:31 -0300

    Added Unit Test for Header

---
+++ src/tests/Header.test.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+import { render } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import Header from '../../src/components/Header';
+
+test('renders editNote component', () => {
+  const store = {
+    getState: jest.fn().mockReturnValue({ trash: [] }),
+    subscribe: jest.fn(),
+    location: jest.fn().mockReturnValue({ location: '' }),
+  };
+
+  const handleToggleDarkMode = jest.fn();
+  const component = render(
+    <Provider store={store}>
+      <Header handleToggleDarkMode={handleToggleDarkMode} />
+    </Provider>,
+  );
+  expect(component).toBeTruthy();
+});
