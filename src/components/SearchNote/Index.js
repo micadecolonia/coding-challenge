29a5f7dcc26d14ebc5c8dbfe92d9719ad31530d4
Micaela Rodriguez <micadecolonia@gmail.com>
2022-06-08 19:13:00 -0300

    Tailwind applied to SearchNote

--- src/components/SearchNote/Index.js
+++ src/components/SearchNote/Index.js
@@ -4,9 +4,9 @@ import { MdSearch } from 'react-icons/md';
 
 function SearchNote({ handleSearchNote }) {
   return (
-    <div className="search">
-      <MdSearch title="Search for a note" className="search-icons" size="1.3em" />
-      <input onChange={(event) => handleSearchNote(event.target.value)} type="text" placeholder="Type in to search for a note" />
+    <div className="flex items-center p-3 rounded-xl mb-6 bg-gray-200">
+      <MdSearch className="rounded-2xl h-7 w-7 text-gray-500 cursor-pointer" title="Search for a note" />
+      <input className="w-80 bg-gray-200 focus:outline-0" onChange={(event) => handleSearchNote(event.target.value)} type="text" placeholder="Type in to search for a note" />
     </div>
   );
 }
