b4b83fdae754597b9a43595b53463ac58b1b2779
Micaela Rodriguez <micadecolonia@gmail.com>
2022-06-08 18:50:22 -0300

    Tailwind applied to Header

--- src/components/Header/Index.js
+++ src/components/Header/Index.js
@@ -21,11 +21,11 @@ function Header({ handleToggleDarkMode }) {
   const { totalNotes } = useSelector((state) => state.trash);
 
   return (
-    <div className="header">
-      <h1 id="page-title">{HeaderView()}</h1>
-      <MdOutlineDarkMode title="Enable or disable dark mode" onClick={() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)} className="moon" />
-      <MdDeleteForever title="Go to the trash screen" onClick={() => history.push('/trash')} className="delete-icon" size="2em" color="red" />
-      <p id="trash-count">{totalNotes}</p>
+    <div className="flex justify-between items-center mt-5 mb-5">
+      <h1 className="flex-[2_2_0%] text-3xl text-black font-bold">{HeaderView()}</h1>
+      <MdOutlineDarkMode className="rounded-2xl h-7 w-7 text-black cursor-pointer hover:bg-gray-200" title="Enable or disable dark mode" onClick={() => handleToggleDarkMode((previousDarkMode) => !previousDarkMode)} />
+      <MdDeleteForever className="rounded-2xl h-7 w-7 text-red-600 cursor-pointer hover:bg-gray-200" title="Go to the trash screen" onClick={() => history.push('/trash')} />
+      <p className="text-lg text-red-600 font-bold">{totalNotes}</p>
     </div>
   );
 }
