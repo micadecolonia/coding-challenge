f5540e7a4a4de5d901a5bd16753944afa19364f1
Micaela Rodriguez <micadecolonia@gmail.com>
2022-06-09 01:57:16 -0300

    Fix btn

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -60,7 +60,7 @@ function Dashboard() {
         )
         : (<AddNote handleAddNote={addN} />)}
       <footer className="flex items-center justify-center mt-2">
-        <button className="text-sm border border-black rounded-2xl p-0.5 cursor-pointer" type="button" onClick={handleClick} title="Click me" size="1.3em">
+        <button className="dark:bg-white text-sm border border-black rounded-2xl p-0.5 cursor-pointer" type="button" onClick={handleClick} title="Click me" size="1.3em">
           {' '}
           {buttonText}
           {' '}
